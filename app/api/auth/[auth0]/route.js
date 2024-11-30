3ce957a0cf0e5896ad62c1328b9cb31d25b85ecd
maclinz <tafadzwamucheke@gmail.com>
2024-11-30 01:04:56 +0000

    js

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,7 +1,7 @@
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export default handleAuth({
-  login: async (req: any, res: any) => {
+  login: async (req, res) => {
     try {
       await handleLogin(req, res, {
         authorizationParams: {
